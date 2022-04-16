93a7ef9faa08b033d07f43729675730b7e734a04
gurk1sh <Gustaf_99@live.se>
2022-04-16 14:23:31 +0200

    Updates

---
+++ precache-manifest.236bf0cd408c9581882aba74b863ea33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe270d5db6c92526df5792745079da27",
+    "url": "/3D-Cars/index.html"
+  },
+  {
+    "revision": "d65dc8e736cc149aec76",
+    "url": "/3D-Cars/static/css/main.959e5259.chunk.css"
+  },
+  {
+    "revision": "9ab4b052caf6ed9ea225",
+    "url": "/3D-Cars/static/js/2.0e729811.chunk.js"
+  },
+  {
+    "revision": "bd16ffa8a645b42adcb7fc65d4c790f8",
+    "url": "/3D-Cars/static/js/2.0e729811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d65dc8e736cc149aec76",
+    "url": "/3D-Cars/static/js/main.9d0d0b59.chunk.js"
+  },
+  {
+    "revision": "02bae55ab66e8d3fa0d5",
+    "url": "/3D-Cars/static/js/runtime-main.cadb5142.js"
+  }
+]);
